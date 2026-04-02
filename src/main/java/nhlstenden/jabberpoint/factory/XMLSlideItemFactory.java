ae85f25081296dd1762265f48c8ec37dc58105a7
Sara Kiani Nejad <sara.kiani.nejad@student.nhlstenden.com>
2026-04-02 15:24:53 +0200

    Added tests for slideitemfactory class and fixed an issue with testing

--- src/main/java/nhlstenden/jabberpoint/factory/XMLSlideItemFactory.java
+++ src/main/java/nhlstenden/jabberpoint/factory/XMLSlideItemFactory.java
@@ -9,7 +9,7 @@ public class XMLSlideItemFactory implements SlideItemFactory {
         return switch (type.toLowerCase()) {
             case "text" -> new TextItem(level, content);
             case "image" -> new BitmapItem(level, content);
-            default -> throw new IllegalArgumentException("Unknown SlideItem type: " + type);
+            default -> throw new IllegalArgumentException("Unknown SlideItem type: ");
         };
     }
 }
