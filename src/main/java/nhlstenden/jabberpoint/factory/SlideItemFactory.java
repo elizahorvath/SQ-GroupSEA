c3c6ecf503cc7c1343e56a40b57ac7f983d7883d
Eliza Horvath <eliza.horvath@student.nhlstenden.com>
2026-04-05 22:04:05 +0200

    edited class diagram

--- src/main/java/nhlstenden/jabberpoint/factory/SlideItemFactory.java
+++
@@ -1,7 +0,0 @@
-package nhlstenden.jabberpoint.factory;
-
-import nhlstenden.jabberpoint.model.SlideItem;
-
-public interface SlideItemFactory {
-    SlideItem createSlideItem(String type, int level, String content);
-}
