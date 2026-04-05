0dee3304b7f58f683f37bed884497dc29a4c72f9
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2026-04-05 21:10:13 +0200

    Feature/observer (#12)
    * edited advice document
    
    * added packages to code
    
    * Re-organised the files in folders
    
    * Removed direct dependency and added add/remove/notify observer methods
    
    * added PresentationObserver interface
    
    * implemented update(Presentation)
    
    * removed code that wont be used anymore
    
    * made clear() public
    
    * Remove manual repaints from MenuController
    
    * Improve XMLAccessor error handling by rethrowing IOException
    
    * update
    
    * fixed by registering it as a Presentation Observer
    
    * Added test for presentation + instance - presentation oberver
    
    * Edited files to match code structure
    
    ---------
    
    Co-authored-by: Eliza Horvath <eliza.horvath@student.nhlstenden.com>

--- src/main/java/nhlstenden/jabberpoint/JabberPoint.java
+++ src/main/java/nhlstenden/jabberpoint/JabberPoint.java
@@ -1,4 +1,5 @@
 package nhlstenden.jabberpoint;
+
 import nhlstenden.jabberpoint.model.Presentation;
 import nhlstenden.jabberpoint.persistance.Accessor;
 import nhlstenden.jabberpoint.persistance.XMLAccessor;
@@ -9,42 +10,45 @@ import javax.swing.JOptionPane;
 
 import java.io.IOException;
 
-/** JabberPoint Main Program
+/**
+ * JabberPoint Main Program
  * <p>This program is distributed under the terms of the accompanying
  * COPYRIGHT.txt file (which is NOT the GNU General Public License).
  * Please read it. Your use of the software constitutes acceptance
  * of the terms in the COPYRIGHT.txt file.</p>
+ *
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 
-public class JabberPoint {
-	protected static final String IOERR = "IO Error: ";
-	protected static final String JABERR = "Jabberpoint Error ";
-	protected static final String JABVERSION = Resources.JABVERSION;
+public class JabberPoint
+{
+    protected static final String IOERR = "IO Error: ";
+    protected static final String JABERR = "Jabberpoint Error ";
+    protected static final String JABVERSION = Resources.JABVERSION;
 
-	/** The Main Program */
-	public static void main(String argv[]) {
-		
-		// Style.createStyles(); - removed line since it is handled by the Singleton now
-		Presentation presentation = new Presentation();
+    /**
+     * The Main Program
+     */
+    public static void main(String argv[])
+    {
+
+        // Style.createStyles(); - removed line since it is handled by the Singleton now
+        Presentation presentation = new Presentation();
         new SlideViewerFrame(JABVERSION, presentation);
-        try {
-            if (argv.length == 0) { // A demo presentation
+        try
+        {
+            if (argv.length == 0)
+            { // A demo presentation
                 Accessor.getDemoAccessor().loadFile(presentation, "");
-            } else {
+            } else
+            {
                 new XMLAccessor().loadFile(presentation, argv[0]);
             }
             presentation.setSlideNumber(0);
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null,
-                    IOERR + ex, JABERR,
-                    JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex)
+        {
+            JOptionPane.showMessageDialog(null, IOERR + ex, JABERR, JOptionPane.ERROR_MESSAGE);
         }
     }
 }
--- src/main/java/nhlstenden/jabberpoint/model/TextItem.java
+++ src/main/java/nhlstenden/jabberpoint/model/TextItem.java
@@ -1,4 +1,5 @@
 package nhlstenden.jabberpoint.model;
+
 import java.awt.Rectangle;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,110 +15,121 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
 
-/** <p>A tekst item.</p>
+/**
+ * <p>A tekst item.</p>
  * <p>A TextItem has drawing functionality.</p>
+ *
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 
-public class TextItem extends SlideItem {
-	private String text;
-	
-	private static final String EMPTYTEXT = "No Text Given";
+public class TextItem extends SlideItem
+{
+    private String text;
 
-	// A text item of a specific level with a text string
-	public TextItem(int level, String string) {
-		super(level);
-		text = string;
-	}
+    private static final String EMPTYTEXT = "No Text Given";
 
-	// An empty text item
-	public TextItem() {
-		this(0, EMPTYTEXT);
-	}
+    // A text item of a specific level with a text string
+    public TextItem(int level, String string)
+    {
+        super(level);
+        text = string;
+    }
 
-	// Returns the text
-	public String getText() {
-		return text == null ? "" : text;
-	}
+    // An empty text item
+    public TextItem()
+    {
+        this(0, EMPTYTEXT);
+    }
 
-	// Returns the AttributedString for the item
-	public AttributedString getAttributedString(Style style, float scale) {
-		AttributedString attrStr = new AttributedString(getText());
-		attrStr.addAttribute(TextAttribute.FONT, style.getFont(scale), 0, text.length());
-		return attrStr;
-	}
+    // Returns the text
+    public String getText()
+    {
+        return text == null ? "" : text;
+    }
 
-	// Returns the bounding box of the item
-	public Rectangle getBoundingBox(Graphics g, ImageObserver observer, 
-			float scale, Style myStyle) {
-		List<TextLayout> layouts = getLayouts(g, myStyle, scale);
-		
-		// Fix: .getLeading() instead of .leading
+    // Returns the AttributedString for the item
+    public AttributedString getAttributedString(Style style, float scale)
+    {
+        AttributedString attrStr = new AttributedString(getText());
+        attrStr.addAttribute(TextAttribute.FONT, style.getFont(scale), 0, text.length());
+
+        return attrStr;
+    }
+
+    // Returns the bounding box of the item
+    public Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style myStyle)
+    {
+        List<TextLayout> layouts = getLayouts(g, myStyle, scale);
+
+        // Fix: .getLeading() instead of .leading
         int xsize = 0, ysize = (int) (myStyle.getLeading() * scale);
-		
-		Iterator<TextLayout> iterator = layouts.iterator();
-		while (iterator.hasNext()) {
-			TextLayout layout = iterator.next();
-			Rectangle2D bounds = layout.getBounds();
-			if (bounds.getWidth() > xsize) {
-				xsize = (int) bounds.getWidth();
-			}
-			if (bounds.getHeight() > 0) {
-				ysize += bounds.getHeight();
-			}
-			ysize += layout.getLeading() + layout.getDescent();
-		}
-		// Fix: .getIndent() instead of .indent
-        return new Rectangle((int) (myStyle.getIndent() * scale), 0, xsize, ysize );
-	}
 
-	// Draws the item
-	public void draw(int x, int y, float scale, Graphics g, 
-			Style myStyle, ImageObserver o) {
-		if (text == null || text.length() == 0) {
-			return;
-		}
-		List<TextLayout> layouts = getLayouts(g, myStyle, scale);
-		
-		// Fix: .getIndent() and .getLeading()
-        Point pen = new Point(x + (int)(myStyle.getIndent() * scale), 
-                y + (int) (myStyle.getLeading() * scale));
-        
-        Graphics2D g2d = (Graphics2D)g;
+        Iterator<TextLayout> iterator = layouts.iterator();
+        while (iterator.hasNext())
+        {
+            TextLayout layout = iterator.next();
+            Rectangle2D bounds = layout.getBounds();
+            if (bounds.getWidth() > xsize)
+            {
+                xsize = (int) bounds.getWidth();
+            }
+            if (bounds.getHeight() > 0)
+            {
+                ysize += bounds.getHeight();
+            }
+            ysize += layout.getLeading() + layout.getDescent();
+        }
+        // Fix: .getIndent() instead of .indent
+        return new Rectangle((int) (myStyle.getIndent() * scale), 0, xsize, ysize);
+    }
+
+    // Draws the item
+    public void draw(int x, int y, float scale, Graphics g, Style myStyle, ImageObserver o)
+    {
+        if (text == null || text.length() == 0)
+        {
+            return;
+        }
+        List<TextLayout> layouts = getLayouts(g, myStyle, scale);
+
+        // Fix: .getIndent() and .getLeading()
+        Point pen = new Point(x + (int) (myStyle.getIndent() * scale), y + (int) (myStyle.getLeading() * scale));
+
+        Graphics2D g2d = (Graphics2D) g;
         // Fix: .getColor() instead of .color
         g2d.setColor(myStyle.getColor());
-		
-		Iterator<TextLayout> it = layouts.iterator();
-		while (it.hasNext()) {
-			TextLayout layout = it.next();
-			pen.y += layout.getAscent();
-			layout.draw(g2d, pen.x, pen.y);
-			pen.y += layout.getDescent();
-		}
-	  }
 
-	private List<TextLayout> getLayouts(Graphics g, Style s, float scale) {
-		List<TextLayout> layouts = new ArrayList<TextLayout>();
-		AttributedString attrStr = getAttributedString(s, scale);
-    	Graphics2D g2d = (Graphics2D) g;
-    	FontRenderContext frc = g2d.getFontRenderContext();
-    	LineBreakMeasurer measurer = new LineBreakMeasurer(attrStr.getIterator(), frc);
-    	// Fix: s.getIndent() instead of s.indent
+        Iterator<TextLayout> it = layouts.iterator();
+        while (it.hasNext())
+        {
+            TextLayout layout = it.next();
+            pen.y += layout.getAscent();
+            layout.draw(g2d, pen.x, pen.y);
+            pen.y += layout.getDescent();
+        }
+    }
+
+    private List<TextLayout> getLayouts(Graphics g, Style s, float scale)
+    {
+        List<TextLayout> layouts = new ArrayList<TextLayout>();
+        AttributedString attrStr = getAttributedString(s, scale);
+        Graphics2D g2d = (Graphics2D) g;
+        FontRenderContext frc = g2d.getFontRenderContext();
+        LineBreakMeasurer measurer = new LineBreakMeasurer(attrStr.getIterator(), frc);
+        // Fix: s.getIndent() instead of s.indent
         float wrappingWidth = (Slide.WIDTH - s.getIndent()) * scale;
-    	while (measurer.getPosition() < getText().length()) {
-    		TextLayout layout = measurer.nextLayout(wrappingWidth);
-    		layouts.add(layout);
-    	}
-    	return layouts;
-	}
+        while (measurer.getPosition() < getText().length())
+        {
+            TextLayout layout = measurer.nextLayout(wrappingWidth);
+            layouts.add(layout);
+        }
 
-	public String toString() {
-		return "TextItem[" + getLevel()+","+getText()+"]";
-	}
+        return layouts;
+    }
+
+    public String toString()
+    {
+        return "TextItem[" + getLevel() + "," + getText() + "]";
+    }
 }
